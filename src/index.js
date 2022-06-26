3d6d59b2d82318b22d6c218f167aeca145253e01
Jessica <duellje@oregonstate.edu>
2022-06-26 15:57:57 -0400

    Finished nav mouseover, working on images expanding and collapsing on doubleclicks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import "./less/index.less";
 
 // Making variables to use:
+//Heading spins 360 degrees when mouse goes over or a link is clicked
+const header = document.querySelector(".logo-heading");
 //Navigation for mouseover effect of bolding
-const nav = document.querySelectorAll(".nav");
+const nav = document.querySelectorAll(".nav-link");
+//Images for expanding and compressing images:
+const images = document.querySelectorAll("img");
+console.log(header);
 
-console.log(nav);
+//Mouse over nav bolds the item the cursor is over, when the mouse leaves the item goes back to normal
+//Two event types used:
+nav.forEach((item) => {
+  item.addEventListener("mouseover", (event) => {
+    event.target.style.fontWeight = "bold";
+  });
 
-const navArray = Array.from(nav);
-console.log(navArray);
+  item.addEventListener("mouseout", (event) => {
+    event.target.style.fontWeight = "normal";
+  });
+});
+
+//When double clicked, images expand. If user used esc key or clicks out of the image, the image will compress back to where it was on the page.
+// ___ event types used:
+images.forEach((img) => {
+  if ((img.style.transform = "scale(1)")) {
+    img.addEventListener("dblclick", (event) => {
+      event.target.style.transform = "scale(1.75)";
+      event.target.style.transition = "transform 0.5s ease";
+    });
+  } else {
+    img.addEventListener("dblclick", (event) => {
+      event.target.style.transform = "scale(1)";
+      event.target.style.transition = "transform 0.5s ease";
+    });
+  }
+});
