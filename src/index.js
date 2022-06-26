cc4db9aea6a0edf9172722333f0d0a1ec22b0b29
Jessica <duellje@oregonstate.edu>
2022-06-26 18:22:00 -0400

    Scaling images in and out is wfunctioning - working on a wheel method'
    '

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const header = document.querySelector(".logo-heading");
 const nav = document.querySelectorAll(".nav-link");
 //Images for expanding and compressing images:
 const images = document.querySelectorAll("img");
-console.log(header);
+const scale = document.querySelector(".scale");
+console.log(scale);
 
 //Mouse over nav bolds the item the cursor is over, when the mouse leaves the item goes back to normal
 //Two event types used:
@@ -22,17 +23,32 @@ nav.forEach((item) => {
 });
 
 //When double clicked, images expand. If user used esc key or clicks out of the image, the image will compress back to where it was on the page.
-// ___ event types used:
+// 2 out of 3 event types used:
 images.forEach((img) => {
-  if ((img.style.transform = "scale(1)")) {
-    img.addEventListener("dblclick", (event) => {
-      event.target.style.transform = "scale(1.75)";
-      event.target.style.transition = "transform 0.5s ease";
-    });
-  } else {
-    img.addEventListener("dblclick", (event) => {
-      event.target.style.transform = "scale(1)";
-      event.target.style.transition = "transform 0.5s ease";
-    });
+  img.addEventListener("dblclick", (event) => {
+    event.target.style.transform = "scale(1.75)";
+    event.target.style.transition = "transform 0.5s ease";
+    event.target.position = "fixed";
+  });
+
+  document.addEventListener("click", () => {
+    img.style.transform = "initial";
+    img.style.transition = "transform 0.5s ease";
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      img.style.transform = "initial";
+    }
+  });
+});
+
+//If zoomed in too much, an alert pops up informing you:
+document.addEventListener("wheel", (event) => {
+  if ((document.style.transform = "scale(3)")) {
+    window.alert("Zoom back out for the best experience.");
   }
 });
+
+//Event types used:
+//Mouseover, mouseout, dblclick, click
