3cfbef5d52f28c19eb11b6338db4f96368bd8fc9
Alex <alex@Jessicas-MBP.attlocal.net>
2022-06-27 14:16:29 -0400

    2 out of 3 sections have double dashed lines - continuing to work on the 3rd

--- src/index.js
+++ src/index.js
@@ -9,11 +9,27 @@ const navLinks = document.querySelectorAll(".nav-link");
 const images = document.querySelectorAll("img");
 //Making another dashed line under navigation:
 const divHeader = document.querySelector(".main-navigation");
-// const headerDash = document.createElement("div");
-const headerDash = document.querySelector(".container.nav-container");
-// divHeader.appendChild(headerDash);
+const dashDiv = document.createElement("div");
+divHeader.appendChild(dashDiv);
+//Making 2nd dashed line under sections:
+const dashBottom = document.querySelectorAll('.intro, .inverse-content');
 
-console.log(headerDash);
+const dashIntro = document.querySelector('.intro');
+const dashDiv2 = document.createElement('div');
+dashIntro.appendChild(dashDiv2);
+
+const dashInverse = document.querySelector('.inverse-content');
+const dashDiv3 = document.createElement('div');
+dashInverse.append(dashDiv3)
+
+//2nd Dashed lines not working here
+const dashTop = document.querySelector('.content-pick');
+const dashDiv4 = document.createElement('div');
+dashTop.prepend(dashDiv4);
+
+
+console.log(dashInverse, dashTop)
+
 
 //Mouse over nav bolds the item the cursor is over, when the mouse leaves the item goes back to normal
 //Two event types used:
@@ -54,12 +70,56 @@ images.forEach((img) => {
 //   }
 // });
 
-//Changing color of initial Header, making it a little bigger:
-header.classList.add("font");
+//Changing color of initial Header, making it a little bigger, adding color:
+header.style.color = '#F8C303';
+header.style.fontSize = '5rem'
+header.style.textShadow = '-2px -2px 0 black, 2px 0 black, -2px 2px 0 black, 2px 2px 0 black'
+header.style.fontWeight = '900'
 
 //Making the header spin:
 
-//Making another dashed line under navigation:
-headerDash.classList.add("dash");
+
+// Making a dash style for the .dash class:
+
+
+//Making another dashed line under navigation + color:
+divHeader.style.paddingBottom = '10px';
+divHeader.style.borderColor = '#E67E22';
+
+dashDiv.style.borderBottom = '2px dashed #E67E22';
+
+images.forEach((item) => {
+    item.style.paddingTop = '20px';
+});
+
+//Making another dashed line under all sections + color:
+dashDiv2.style.borderTop = '2px dashed #E67E22'
+
+dashBottom.forEach((item) => {
+    item.style.paddingBottom = '10px';
+    item.style.borderBottom = '2px dashed #E67E22';
+})
+
+dashDiv3.style.paddingTop = '10px'
+dashDiv3.style.borderBottom = '2px dashed #E67E22';
+dashDiv3.style.width = '100%'
+
+dashTop.style.paddingTop = '10px';
+dashTop.style.borderTop = '2px dashed #E67E22';
+dashTop.style.justifyContent = 'center'
+
+dashDiv4.style.alignItems = 'flex-start'
+dashDiv4.style.paddingTop = '10px';
+dashDiv4.style.width = '100%';
+dashDiv4.style.border = '10px dashed #E67E22';
+
+// dashDiv2.style.border = '5px solid'
+// dashSections.forEach((item) => {
+//     item.style.borderTop = '2px dash #E67E22' 
+//     dashDiv2.classList.add('dash')
+// dashDiv2.style.borderTop = '2px dash #E67E22 @navigation-border'
+//     // item.style.border = '5px solid'
+// })
+
 //Event types used:
 //Mouseover, mouseout, dblclick, click, keydown
