bee0a63cdb89ad142f005a3d4b33a6e85cd6bdcd
Alex <alex@Jessicas-MBP.attlocal.net>
2022-06-27 18:18:00 -0400

    Added links to sign up buttons - working on changing footer styles

--- src/index.js
+++ src/index.js
@@ -12,23 +12,34 @@ const divHeader = document.querySelector(".main-navigation");
 const dashDiv = document.createElement("div");
 divHeader.appendChild(dashDiv);
 //Making 2nd dashed line under sections:
-const dashBottom = document.querySelectorAll('.intro, .inverse-content');
-
+//for first set of double dashed lines
 const dashIntro = document.querySelector('.intro');
 const dashDiv2 = document.createElement('div');
 dashIntro.appendChild(dashDiv2);
 
+//for second set of double dashed lines
 const dashInverse = document.querySelector('.inverse-content');
 const dashDiv3 = document.createElement('div');
 dashInverse.append(dashDiv3)
 
-//2nd Dashed lines not working here
-const dashTop = document.querySelector('.content-pick');
+//for last set of double dashed lines
+const dashPick = document.querySelector('.content-pick');
 const dashDiv4 = document.createElement('div');
-dashTop.prepend(dashDiv4);
+dashPick.append(dashDiv4);
+const dashPickText = document.querySelectorAll('.content-pick .destination')
+dashDiv4.append(dashPickText[0])
+dashDiv4.append(dashPickText[1])
+dashDiv4.append(dashPickText[2])
 
+// Double dashed lines for footer:
+const footer = document.querySelector('footer');
+const divFooter = document.createElement('div')
+footer.prepend(divFooter)
 
-console.log(dashInverse, dashTop)
+// For preventDefault to change Sign Up button links to go somewhere:
+const btnLinks = document.querySelectorAll('.content-pick .destination .btn')
+
+console.log(footer)
 
 
 //Mouse over nav bolds the item the cursor is over, when the mouse leaves the item goes back to normal
@@ -63,23 +74,13 @@ images.forEach((img) => {
   });
 });
 
-//If zoomed in too much, an alert pops up informing you:
-// document.addEventListener("wheel", (event) => {
-//   if ((event.target.style.transform = "scale(10)")) {
-//     alert("Zoom back out for the best experience.");
-//   }
-// });
-
 //Changing color of initial Header, making it a little bigger, adding color:
 header.style.color = '#F8C303';
 header.style.fontSize = '5rem'
 header.style.textShadow = '-2px -2px 0 black, 2px 0 black, -2px 2px 0 black, 2px 2px 0 black'
 header.style.fontWeight = '900'
 
-//Making the header spin:
-
-
-// Making a dash style for the .dash class:
+//Making the header spin - if I have time:
 
 
 //Making another dashed line under navigation + color:
@@ -93,33 +94,63 @@ images.forEach((item) => {
 });
 
 //Making another dashed line under all sections + color:
+//1st set of double dashed lines
+dashIntro.style.borderBottom = '2px dashed #E67E22';
+dashIntro.style.paddingBottom = '10px';
 dashDiv2.style.borderTop = '2px dashed #E67E22'
 
-dashBottom.forEach((item) => {
-    item.style.paddingBottom = '10px';
-    item.style.borderBottom = '2px dashed #E67E22';
-})
-
+//2nd set of double dashed lines 
+dashInverse.style.borderBottom = '2px dashed #E67E22';
+dashInverse.style.paddingBottom = '10px';
 dashDiv3.style.paddingTop = '10px'
 dashDiv3.style.borderBottom = '2px dashed #E67E22';
 dashDiv3.style.width = '100%'
 
-dashTop.style.paddingTop = '10px';
-dashTop.style.borderTop = '2px dashed #E67E22';
-dashTop.style.justifyContent = 'center'
+//Last set of double dashed lines
+dashPick.style.paddingTop = '10px';
+dashPick.style.borderTop = '2px dashed #E67E22';
 
-dashDiv4.style.alignItems = 'flex-start'
+dashDiv4.style.display = 'flex'
+dashDiv4.style.justifyContent = 'space-between'
 dashDiv4.style.paddingTop = '10px';
 dashDiv4.style.width = '100%';
-dashDiv4.style.border = '10px dashed #E67E22';
+dashDiv4.style.borderTop = '2px dashed #E67E22';
 
-// dashDiv2.style.border = '5px solid'
-// dashSections.forEach((item) => {
-//     item.style.borderTop = '2px dash #E67E22' 
-//     dashDiv2.classList.add('dash')
-// dashDiv2.style.borderTop = '2px dash #E67E22 @navigation-border'
-//     // item.style.border = '5px solid'
-// })
+//Footer double dashed lines:
+divFooter.style.width = '100%'
+divFooter.style.borderTop = '2px dashed #E67E22';
+footer.style.paddingTop = '10px'
+footer.style.borderTop = '2px dashed #E67E22'
 
+//Changing footer background color and footer text color:
+footer.style.backgroundColor = '#090B0F';
+footer.style.color = '#4E4F51'
 //Event types used:
 //Mouseover, mouseout, dblclick, click, keydown
+
+//Styles used: 
+//Border, width, padding, color, borderColor, textShadow, fontSize, fontWeight, transform, transition, display, justifyContent
+
+//preventDefault to make links go somewhere:
+// Making a class for each button on their divs:
+// btnLinks[0].classList.add('one')
+// btnLinks[1].classList.add('two')
+// btnLinks[2].classList.add('three')
+
+//Adding links to each button:
+btnLinks[0].addEventListener('click', () => {
+    window.open('https://portal.ehawaii.gov/#visitors', '_blank');
+})
+
+btnLinks[1].addEventListener('click', () => {
+    window.open('https://www.highlandschamber.org/?_ga-ft=1YkYYn.0.0.0.0.G5Q79N-1PMDd9K.0.0', '_blank')
+})
+
+btnLinks[2].addEventListener('click', () => {
+    window.open('https://www.igetaway.net/', '_blank')
+})
+
+
+
+console.log(btnLinks[0])
+console.log(btnLinks)
